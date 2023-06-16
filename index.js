385534f779e16f069126f1274a6ccd45f8a0f351
shantdashjian <shantgdashjian@gmail.com>
2023-06-16 20:18:19 +0400

    Add light/dark theme switcher and fix inconsistent indentation across all files

--- index.js
+++ index.js
@@ -7,23 +7,50 @@ const convertBtn = document.getElementById("convert-btn")
 const lengthTextEl = document.getElementById("length-text-el")
 const volumeTextEl = document.getElementById("volume-text-el")
 const massTextEl = document.getElementById("mass-text-el")
+const themeEl = document.getElementById('theme')
+
+const userPrefersDarkColorScheme = window.matchMedia('(prefers-color-scheme: dark)')
 
 convertBtn.addEventListener("click", () => {
-    const number = numberEl.value
-    lengthTextEl.textContent = buildConversionText(number, metersToFeetRate, 'meters', 'feet')
-    volumeTextEl.textContent = buildConversionText(number, litersToGallonsRate, 'liters', 'gallons')
-    massTextEl.textContent = buildConversionText(number, kilogramsToPoundsRate, 'kilos', 'pounds')
+	const number = numberEl.value
+	lengthTextEl.textContent = buildConversionText(number, metersToFeetRate, 'meters', 'feet')
+	volumeTextEl.textContent = buildConversionText(number, litersToGallonsRate, 'liters', 'gallons')
+	massTextEl.textContent = buildConversionText(number, kilogramsToPoundsRate, 'kilos', 'pounds')
 })
 
 function buildConversionText(number, conversionRate, metricUnit, imperialUnit) {
-    const fromMetricToImperialUnit = number * conversionRate
-    const fromImperialToMetricUnit = number / conversionRate
-    return `${number} ${metricUnit} = ${fromMetricToImperialUnit.toFixed(3)} ${imperialUnit} | ${number} ${imperialUnit} = ${fromImperialToMetricUnit.toFixed(3)} ${metricUnit}`
+	const fromMetricToImperialUnit = number * conversionRate
+	const fromImperialToMetricUnit = number / conversionRate
+	return `${number} ${metricUnit} = ${fromMetricToImperialUnit.toFixed(3)} ${imperialUnit} | ${number} ${imperialUnit} = ${fromImperialToMetricUnit.toFixed(3)} ${metricUnit}`
 }
 
 function numberizeAndResizeInputField() {
-    if (isNaN(numberEl.value) || numberEl.value < 0) {
-        numberEl.value = "0"
-    }
-    numberEl.style.width = (numberEl.value.length + 3) + "ch";
+	if (isNaN(numberEl.value) || numberEl.value < 0) {
+		numberEl.value = "0"
+	}
+	numberEl.style.width = (numberEl.value.length + 3) + "ch";
 }
+
+function makeThemeDark(dark) {
+	if (dark) {
+		document.body.classList.add('dark')
+		themeEl.innerHTML = '<i class="fas fa-moon theme-icon"></i>'
+	} else {
+		document.body.classList.remove('dark')
+		themeEl.innerHTML = '<i class="fas fa-sun theme-icon"></i>'
+	}
+}
+
+themeEl.addEventListener('click', () => {
+	if (document.body.classList.contains('dark')) {
+		makeThemeDark(false)
+	} else {
+		makeThemeDark(true)
+	}
+})
+
+makeThemeDark(userPrefersDarkColorScheme?.matches)
+
+userPrefersDarkColorScheme?.addEventListener('change', event => {
+	makeThemeDark(event.matches)
+})
